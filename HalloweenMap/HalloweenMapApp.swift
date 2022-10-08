7a17ef044d5315af9275ab7e6127d160019df221
anarraidoo <32473241+anarraidoo@users.noreply.github.com>
2022-10-08 11:39:17 -0400

    Initial Commit

---
+++ HalloweenMap/HalloweenMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalloweenMapApp.swift
+//  HalloweenMap
+//
+//  Created by Anusha Narraidoo on 2022-10-08.
+//
+
+import SwiftUI
+
+@main
+struct HalloweenMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
